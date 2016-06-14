6c3b27d37c1c4fd97a3cab467e936b032e3319ac
Robert Smol <robert.smol@stereoit.com>
2016-06-14 10:19:20 +0200

    Added support for React bundling

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,18 @@
 import buble from 'rollup-plugin-buble';
 import uglify from 'rollup-plugin-uglify';
+import filesize from 'rollup-plugin-filesize';
+import nodeResolve	from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+
 
 export default {
 	entry: 'src/app.js',
 	dest: 'dist/bundle.js',
-	plugins: [ buble(), uglify() ],
+	plugins: [
+		replace({'process.env.NODE_ENV':'"production"'}),
+		buble(), uglify(), filesize(), commonjs(),
+		nodeResolve( { jsnext: true, main: true} )
+	 ],
 	format: 'iife'
 };
